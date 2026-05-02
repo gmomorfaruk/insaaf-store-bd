a102e7ae20f35034bda667098f63d3ed2985b2cb
Md Omor Faruk <189721858+gmomorfaruk@users.noreply.github.com>
2026-05-02 13:37:49 +0600

    New updated Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -359,17 +359,20 @@
 
 
 
+"use client";
 
-return (
-  <main style={{
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    fontFamily: "sans-serif"
-  }}>
-    <h1>🚧 Site Under Maintenance</h1>
-    <p>We’ll be back soon.</p>
-  </main>
-);
+export default function Home() {
+  return (
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>🚧 Site Under Maintenance</h1>
+      <p>We’ll be back soon.</p>
+    </main>
+  );
+}
